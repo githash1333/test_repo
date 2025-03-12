84d7eb957471c7c601b0d9a9c84c4925a004ffc0
githash1333 <gitshash45@gmail.com>
2025-03-12 17:51:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-import webview
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
-class MyApp(App):
+class HelloWorldApp(App):
     def build(self):
-        # Open the web view directly when the app starts
-        self.open_webview()
-        return BoxLayout()  # Return an empty layout since we don't need any Kivy widgets
-
-    def open_webview(self):
-        # Open the specified URL in a new web view window
-        webview.create_window('Firstbytebox', 'http://192.168.86.1:8501/')  # Replace with your desired URL
-        webview.start()
+        return Label(text='Hello, World!')
 
 if __name__ == '__main__':
-    MyApp().run()
-
-#http://192.168.86.1:8501/
-#pip install git+https://github.com/kivy-garden/garden.webview.git
-# !pip install kivy
-# !pip install kivymd
-# !pip install kivy_garden
-# !pip install kivy-garden.xcamera==2020.613
+    HelloWorldApp().run()
