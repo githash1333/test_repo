6adb8f60a9274fae2766c06b31e3e45c9ed88468
githash1333 <gitshash45@gmail.com>
2025-03-12 19:58:16 +0530

    commit_push

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# app.py
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +8,15 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    # Check if running as a bundled executable
+    if getattr(sys, 'frozen', False):
+        # If the application is frozen, use the directory of the executable
+        base_path = sys._MEIPASS
+    else:
+        # If not frozen, use the current directory
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    # Set the template folder
+    app.template_folder = os.path.join(base_path, 'templates')
+
     app.run(debug=True)
